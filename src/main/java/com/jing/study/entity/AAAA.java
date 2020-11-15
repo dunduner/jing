327e9a0bf6cf44a6447f1cbe0abcecb18f2c32b8
sunning <ning11.zhang@zhangmen.com>
2020-11-15 14:06:55 +0800

    AAA

---
+++ src/main/java/com/jing/study/entity/AAAA.java
@@ -0,0 +1,12 @@
+package com.jing.study.entity;
+
+/**
+ * @author ning11.zhang
+ * @Description:
+ * @date 2020/11/15
+ */
+public class AAAA {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
